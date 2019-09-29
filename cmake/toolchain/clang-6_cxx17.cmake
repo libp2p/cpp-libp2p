e2d83af33f207e1c00eb1926b4a0b3b5dddc387f
Bogdan Vaneev <warchantua@gmail.com>
2019-09-29 17:44:33 +0300

    Remove toolchain
    Signed-off-by: Bogdan Vaneev <warchantua@gmail.com>

--- cmake/toolchain/clang-6_cxx17.cmake
+++
@@ -1,3 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/../print.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/compiler/clang-6.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/cxx17.cmake)
--- cmake/toolchain/gcc-7_cxx17.cmake
+++
@@ -1,3 +0,0 @@
-include(${CMAKE_CURRENT_LIST_DIR}/../print.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/compiler/gcc-7.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/cxx17.cmake)
