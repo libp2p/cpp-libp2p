d503dfdcdfb353fc83761329c2ac4d0106392580
Artem <gorbachev@soramitsu.co.jp>
2019-10-23 09:09:27 +0300

    cmake hunter config fix for gtest and gcc-9

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -8,3 +8,9 @@ hunter_config(GSL
     SHA1 9bbdea551b38d7d09ab7aa2e89b91a66dd032b4a
     CMAKE_ARGS GSL_TEST=OFF
     )
+
+hunter_config(
+    GTest
+    VERSION 1.8.0-hunter-p11
+    CMAKE_ARGS "CMAKE_CXX_FLAGS=-Wno-deprecated-copy"
+)
