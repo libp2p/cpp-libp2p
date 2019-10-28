ae1aae8a8c9924da635fced9decc8f09e4dd298c
Yura Zarudniy <zarudniy@soramitsu.co.jp>
2019-10-28 16:05:57 +0300

    remove redundant operator

--- include/libp2p/common/logger.hpp
+++ include/libp2p/common/logger.hpp
@@ -9,14 +9,6 @@
 #include <spdlog/fmt/ostr.h>
 #include <spdlog/spdlog.h>
 
-/// Allows to log objects, which have toString() method without calling it, e.g.
-/// log.info("{}", myObject)
-template <typename StreamType, typename T>
-auto operator<<(StreamType &os, const T &object)
-    -> decltype(os << object.toString()) {
-  return os << object.toString();
-}
-
 namespace libp2p::common {
   using Logger = std::shared_ptr<spdlog::logger>;
 
