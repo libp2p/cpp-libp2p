b36fff4d01a6ea356f70025488ffc7a40b15188e
Sergey Kaprovich <serginetty@gmail.com>
2019-11-29 16:26:30 +0300

    Secp256k1 crypto provider (#25)
    Signed-off-by: Sergey Kaprovich <serginetty@gmail.com>

---
+++ include/libp2p/crypto/secp256k1_types.hpp
@@ -0,0 +1,47 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef LIBP2P_CRYPTO_SECP256K1_TYPES_HPP
+#define LIBP2P_CRYPTO_SECP256K1_TYPES_HPP
+
+#include <vector>
+#include <array>
+
+namespace libp2p::crypto::secp256k1 {
+  /**
+   * @brief Common types
+   */
+  using PrivateKey = std::array<uint8_t, 32>;
+  using PublicKey = std::array<uint8_t, 33>;
+  using Signature = std::vector<uint8_t>;
+
+  /**
+   * @struct Key pair
+   */
+  struct KeyPair {
+    PrivateKey private_key; /**< Secp256k1 private key */
+    PublicKey public_key;   /**< Secp256k1 public key */
+
+    /**
+     * @brief Comparing keypairs
+     * @param other - second keypair to compare
+     * @return true, if keypairs are equal
+     */
+    bool operator==(const KeyPair &other) {
+      return private_key == other.private_key && public_key == other.public_key;
+    }
+
+    /**
+     * @brief Comparing keypairs
+     * @param other - second keypair to compare
+     * @return true, if keypairs aren't equal
+     */
+    bool operator!=(const KeyPair &other) {
+      return !operator==(other);
+    }
+  };
+};  // namespace libp2p::crypto::secp256k1
+
+#endif  // LIBP2P_CRYPTO_SECP256K1_TYPES_HPP
---
+++ src/crypto/secp256k1_provider/secp256k1_provider_impl.cpp
@@ -0,0 +1,125 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include "libp2p/crypto/secp256k1_provider/secp256k1_provider_impl.hpp"
+
+#include <openssl/bn.h>
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h>
+
+#include <memory>
+
+#include "libp2p/crypto/error.hpp"
+#include "libp2p/crypto/sha/sha256.hpp"
+#include "libp2p/crypto/common_functions.hpp"
+
+namespace libp2p::crypto::secp256k1 {
+  outcome::result<KeyPair> Secp256k1ProviderImpl::generateKeyPair() const {
+    PublicKey public_key{};
+    PrivateKey private_key{};
+    std::shared_ptr<EC_KEY> key{EC_KEY_new_by_curve_name(NID_secp256k1),
+                                EC_KEY_free};
+    if (EC_KEY_generate_key(key.get()) != 1) {
+      return KeyGeneratorError::KEY_GENERATION_FAILED;
+    }
+    const BIGNUM *privateNum = EC_KEY_get0_private_key(key.get());
+    if (BN_bn2binpad(privateNum, private_key.data(), private_key.size()) < 0) {
+      return KeyGeneratorError::KEY_GENERATION_FAILED;
+    }
+    EC_KEY_set_conv_form(key.get(), POINT_CONVERSION_COMPRESSED);
+    auto public_key_length = i2o_ECPublicKey(key.get(), nullptr);
+    if (public_key_length != public_key.size()) {
+      return KeyGeneratorError::KEY_GENERATION_FAILED;
+    }
+    uint8_t *public_key_ptr = public_key.data();
+    if (i2o_ECPublicKey(key.get(), &public_key_ptr) != public_key_length) {
+      return KeyGeneratorError::KEY_GENERATION_FAILED;
+    }
+    return KeyPair{private_key, public_key};
+  }
+
+  outcome::result<PublicKey> Secp256k1ProviderImpl::derivePublicKey(
+      const PrivateKey &key) const {
+    OUTCOME_TRY(private_key, bytesToPrivateKey(key));
+    PublicKey public_key{};
+    uint8_t *public_key_ptr = public_key.data();
+    ssize_t generated_length = i2o_ECPublicKey(private_key.get(), nullptr);
+    if (generated_length != public_key.size()) {
+      return KeyGeneratorError::KEY_GENERATION_FAILED;
+    }
+    i2o_ECPublicKey(private_key.get(), &public_key_ptr);
+    return public_key;
+  }
+
+  outcome::result<Signature> Secp256k1ProviderImpl::sign(
+      gsl::span<uint8_t> message, const PrivateKey &key) const {
+    common::Hash256 digest = sha256(message);
+    OUTCOME_TRY(private_key, bytesToPrivateKey(key));
+    OUTCOME_TRY(signature, GenerateEcSignature(digest, private_key));
+    return std::move(signature);
+  }
+
+  outcome::result<bool> Secp256k1ProviderImpl::verify(
+      gsl::span<uint8_t> message, const Signature &signature,
+      const PublicKey &key) const {
+    common::Hash256 digest = sha256(message);
+    OUTCOME_TRY(public_key, bytesToPublicKey(key));
+    OUTCOME_TRY(result, VerifyEcSignature(digest, signature, public_key));
+    return result;
+  }
+
+  outcome::result<std::shared_ptr<EC_KEY>>
+  Secp256k1ProviderImpl::bytesToPrivateKey(const PrivateKey &input) {
+    std::shared_ptr<EC_KEY> key{EC_KEY_new_by_curve_name(NID_secp256k1),
+                                EC_KEY_free};
+    if (key.get() == nullptr) {
+      return KeyGeneratorError::INTERNAL_ERROR;
+    }
+    std::unique_ptr<BIGNUM, void (*)(BIGNUM *)> key_bignum{
+        BN_bin2bn(input.data(), input.size(), nullptr), BN_free};
+    if (key_bignum == nullptr) {
+      return KeyGeneratorError::INTERNAL_ERROR;
+    }
+    if (EC_KEY_set_private_key(key.get(), key_bignum.get()) != 1) {
+      return KeyGeneratorError::INTERNAL_ERROR;
+    }
+    std::unique_ptr<EC_POINT, void (*)(EC_POINT *)> point{
+        EC_POINT_new(EC_KEY_get0_group(key.get())), EC_POINT_free};
+    if (point == nullptr) {
+      return KeyGeneratorError::INTERNAL_ERROR;
+    }
+    EC_KEY_set_conv_form(key.get(), POINT_CONVERSION_COMPRESSED);
+    if (EC_POINT_mul(EC_KEY_get0_group(key.get()), point.get(),
+                     key_bignum.get(), nullptr, nullptr, nullptr)
+        != 1) {
+      return KeyGeneratorError::INTERNAL_ERROR;
+    }
+    if (EC_KEY_set_public_key(key.get(), point.get()) != 1) {
+      return KeyGeneratorError::INTERNAL_ERROR;
+    }
+    if (EC_KEY_check_key(key.get()) != 1) {
+      return KeyValidatorError::INVALID_PRIVATE_KEY;
+    }
+    return key;
+  }
+
+  outcome::result<std::shared_ptr<EC_KEY>>
+  Secp256k1ProviderImpl::bytesToPublicKey(const PublicKey &input) {
+    std::shared_ptr<EC_KEY> key{EC_KEY_new_by_curve_name(NID_secp256k1),
+                                EC_KEY_free};
+    if (key == nullptr) {
+      return KeyGeneratorError::INTERNAL_ERROR;
+    }
+    const uint8_t *public_key_ptr = input.data();
+    EC_KEY *key_ptr = key.get();
+    key_ptr = o2i_ECPublicKey(&key_ptr, &public_key_ptr, input.size());
+    if (key_ptr == nullptr) {
+      return KeyGeneratorError::INTERNAL_ERROR;
+    }
+    if (EC_KEY_check_key(key.get()) != 1) {
+      return KeyValidatorError::INVALID_PUBLIC_KEY;
+    }
+    return key;
+  }
+}  // namespace libp2p::crypto::secp256k1
