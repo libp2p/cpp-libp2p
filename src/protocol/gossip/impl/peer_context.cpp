9529fdb53533b9354d604ac0c0103ca2dd49835a
Artem <artgor4github@gmail.com>
2019-12-30 13:04:32 +0300

    gossip: renmings

---
+++ src/protocol/gossip/impl/peer_context.cpp
@@ -0,0 +1,28 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <libp2p/protocol/gossip/impl/peer_context.hpp>
+
+namespace libp2p::protocol::gossip {
+
+  bool operator<(const PeerContextPtr &ctx, const peer::PeerId &peer) {
+    if (!ctx)
+      return false;
+    return less(ctx->peer_id, peer);
+  }
+
+  bool operator<(const peer::PeerId &peer, const PeerContextPtr &ctx) {
+    if (!ctx)
+      return false;
+    return less(peer, ctx->peer_id);
+  }
+
+  bool operator<(const PeerContextPtr &a, const PeerContextPtr &b) {
+    if (!a || !b)
+      return false;
+    return less(a->peer_id, b->peer_id);
+  }
+
+}  // namespace libp2p::protocol::gossip
