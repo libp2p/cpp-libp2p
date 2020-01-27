cdc092fe74ef5d8eeedf42e3e949488ff5052cb4
Yura Zarudniy <zarudniy@soramitsu.co.jp>
2020-01-27 11:09:29 +0300

    Fix multihash (#53)
    * fix multihash
    
    Signed-off-by: Yura Zarudniy <zarudniy@soramitsu.co.jp>
    
    * clean up code
    
    Signed-off-by: Yura Zarudniy <zarudniy@soramitsu.co.jp>

--- include/libp2p/multi/multihash.hpp
+++ include/libp2p/multi/multihash.hpp
@@ -111,8 +111,7 @@ namespace libp2p::multi {
      * hash itself
      */
     std::vector<uint8_t> data_;
-    // does not store hash itself, points on data_
-    gsl::span<const uint8_t> hash_;
+    uint8_t hash_offset_{};  ///< size of non-hash data from the beginning
     HashType type_;
   };
 
