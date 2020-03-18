f9e9a908ef792f1c1b72be1816ad1feae880ef4c
Igor Egorov <igor@soramitsu.co.jp>
2020-03-18 14:10:18 +0300

    Add SQLite wrapper
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -34,3 +34,9 @@ hunter_config(
     # disable building examples and tests, disable testing
     CMAKE_ARGS BOOST_DI_OPT_BUILD_TESTS=OFF BOOST_DI_OPT_BUILD_EXAMPLES=OFF
 )
+
+hunter_config(
+    SQLiteModernCpp
+    URL https://github.com/soramitsu/libp2p-sqlite-modern-cpp/archive/fc3b700064cb57ab6b598c9bc7a12b2842f78da2.zip
+    SHA1 d913f2a0360892a30bc7cd8820a0475800b47d76
+)
