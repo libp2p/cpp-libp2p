5ff9050a330b1095780d0e52a25e4cd650bf7770
Alexey <Alexey.N.Chernyshov@gmail.com>
2020-06-19 10:46:25 +0300

    Fix/asio scheduler (#79)
    * test - AsioScheduler called onTimer after being destructed
    
    Signed-off-by: Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>
    
    * fix AsioScheduler
    
    Signed-off-by: Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>
    
    * add comment
    
    Signed-off-by: Alexey-N-Chernyshov <alexey.n.chernyshov@gmail.com>

---
+++ test/libp2p/protocol/common/asio/asio_scheduler_test.cpp
@@ -0,0 +1,27 @@
+/**
+ * Copyright Soramitsu Co., Ltd. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "libp2p/protocol/common/asio/asio_scheduler.hpp"
+
+#include <gtest/gtest.h>
+#include <libp2p/injector/host_injector.hpp>
+
+using libp2p::protocol::Scheduler;
+
+/**
+ * @given Constructs AsioScheduler and schedules on io context and then deletes
+ * AsioScheduler
+ * @when context is run
+ * @then scheduler called and can handle cancellation timer without segfault
+ */
+TEST(AsioScheduler, Construct) {
+  auto injector = libp2p::injector::makeHostInjector();
+  auto context = injector.create<std::shared_ptr<boost::asio::io_context>>();
+  std::shared_ptr<Scheduler> scheduler =
+      std::make_shared<libp2p::protocol::AsioScheduler>(
+          *context, libp2p::protocol::SchedulerConfig{});
+  scheduler.reset();
+  context->run();
+}
