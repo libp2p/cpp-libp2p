f62a508009d25744b4f10dfc9196197416f8ba92
Igor Egorov <igor@soramitsu.co.jp>
2021-02-25 11:54:57 +0300

    Enable Code Coverage (#112)
    Signed-off-by: Igor Egorov <igor@soramitsu.co.jp>

--- cmake/functions.cmake
+++ cmake/functions.cmake
@@ -22,6 +22,7 @@ function(addtest test_name)
       LIBRARY_OUTPUT_PATH ${CMAKE_BINARY_DIR}/test_lib
       )
   disable_clang_tidy(${test_name})
+  set_property(GLOBAL APPEND PROPERTY TEST_TARGETS ${test_name})
 endfunction()
 
 function(addtest_part test_name)
