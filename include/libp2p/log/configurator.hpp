972c2aa42fd77340cba0b9be1c8e69e0e3bd96d4
Dmitriy Khaustov <khaustov.dm@gmail.com>
2021-03-26 09:40:31 +0300

    Fix default logger configurator at injection (#120)
    ￼*fix: force to use default ctor of logger configurator at injection
    
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- include/libp2p/log/configurator.hpp
+++ include/libp2p/log/configurator.hpp
@@ -8,10 +8,14 @@
 
 #include <soralog/impl/configurator_from_yaml.hpp>
 
+#include <boost/di.hpp>
+
 namespace libp2p::log {
 
   class Configurator : public soralog::ConfiguratorFromYAML {
    public:
+    BOOST_DI_INJECT_TRAITS();
+
     Configurator();
 
     explicit Configurator(std::string config);
