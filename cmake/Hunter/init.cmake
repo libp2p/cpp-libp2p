f6104193ad01bf8caef89fd6a15226f968dfbac7
Dmitriy Khaustov <khaustov.dm@gmail.com>
2021-03-31 19:23:23 +0300

    Sync logging with updated soralog (#125)
    * feature: sync logging
    * update: bump hunter version
    
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -31,7 +31,7 @@ set(
 include(${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake)
 
 HunterGate(
-    URL  "https://github.com/soramitsu/soramitsu-hunter/archive/v0.23.257-soramitsu4.tar.gz"
-    SHA1 "8c277ce015d4b4a0a0cc22ef9ba6da5962e4fc0a"
+    URL  "https://github.com/soramitsu/soramitsu-hunter/archive/v0.23.257-soramitsu5.tar.gz"
+    SHA1 "e95997ef66f531ed294150b10abc775483d94a61"
     LOCAL
 )
