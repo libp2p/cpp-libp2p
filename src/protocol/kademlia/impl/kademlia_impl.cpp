47f86a854b5a1033f01c82e3ad6d18dc0cfdd3f8
ortyomka <iurin.art@gmail.com>
2022-08-24 16:38:25 +0400

    Add response for PutValue
    Signed-off-by: ortyomka <iurin.art@gmail.com>

--- src/protocol/kademlia/impl/kademlia_impl.cpp
+++ src/protocol/kademlia/impl/kademlia_impl.cpp
@@ -317,6 +317,15 @@ namespace libp2p::protocol::kademlia {
       log_.warn("incoming PutValue failed: {}", res.error().message());
       return;
     }
+
+    // echo request
+    auto buffer = std::make_shared<std::vector<uint8_t>>();
+    if (not msg.serialize(*buffer)) {
+      session->close(Error::MESSAGE_SERIALIZE_ERROR);
+      BOOST_UNREACHABLE_RETURN();
+    }
+
+    session->write(buffer, {});
   }
 
   void KademliaImpl::onGetValue(const std::shared_ptr<Session> &session,
