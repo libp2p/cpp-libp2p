a14ad6a74336ee754c54fc230e2b12e04c3cd966
kamilsa <kamilsa16@gmail.com>
2023-09-06 19:06:23 +0600

    Update hunter links

--- cmake/Hunter/passwords.cmake
+++ cmake/Hunter/passwords.cmake
@@ -1,9 +1,8 @@
 hunter_upload_password(
     # REPO_OWNER + REPO = https://github.com/forexample/hunter-cache
-    REPO_OWNER "soramitsu"
+    REPO_OWNER "qdrvm"
     REPO "hunter-binary-cache"
 
-    # USERNAME = warchant
     USERNAME "$ENV{GITHUB_HUNTER_USERNAME}"
 
     # PASSWORD = GitHub token saved as a secure environment variable
