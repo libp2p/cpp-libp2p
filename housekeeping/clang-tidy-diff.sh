8b23361f7b536c6d7b62354722bfc910d4206808
turuslan <turuslan.devbox@gmail.com>
2024-04-03 21:14:11 +0500

    clang-tidy bug
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- housekeeping/clang-tidy-diff.sh
+++ housekeeping/clang-tidy-diff.sh
@@ -8,4 +8,5 @@
 BUILD_DIR="${BUILD_DIR:?}"
 
 cd $(dirname $0)/..
-git diff -U0 origin/master | clang-tidy-diff.py -p1 -path $BUILD_DIR -iregex '(include|src)\/.*\.(h|c|hpp|cpp)'
+git diff -U0 origin/master | clang-tidy-diff.py -p1 -path $BUILD_DIR -iregex '(include|src)\/.*\.(h|c|hpp|cpp)' | tee clang-tidy.log
+grep -v ': error:' clang-tidy.log
