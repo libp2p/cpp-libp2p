c1d251d2622b654c636208abb233a43b349a2e5c
turuslan <turuslan.devbox@gmail.com>
2024-04-03 09:22:34 +0500

    try fix ci
    Signed-off-by: turuslan <turuslan.devbox@gmail.com>

--- housekeeping/clang-tidy-diff.sh
+++ housekeeping/clang-tidy-diff.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-BUILD_DIR="${BUILD_DIR:-build}"
+BUILD_DIR="${BUILD_DIR:?}"
 
 cd $(dirname $0)/..
 git diff -U0 HEAD^ | clang-tidy-diff.py -p1 -path $BUILD_DIR -regex "\.(hpp|h)"
