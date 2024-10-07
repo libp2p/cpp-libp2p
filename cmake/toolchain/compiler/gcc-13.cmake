3b5dda2fec9e70d76a2364f346446a396791900a
kamilsa <kamilsa16@gmail.com>
2024-10-07 13:24:34 +0500

    Bump to gcc-13

--- cmake/toolchain/compiler/gcc-13.cmake
+++ cmake/toolchain/compiler/gcc-13.cmake
@@ -4,15 +4,15 @@ else()
   set(POLLY_COMPILER_GCC_8_CMAKE_ 1)
 endif()
 
-find_program(CMAKE_C_COMPILER gcc-11)
-find_program(CMAKE_CXX_COMPILER g++-11)
+find_program(CMAKE_C_COMPILER gcc-13)
+find_program(CMAKE_CXX_COMPILER g++-13)
 
 if(NOT CMAKE_C_COMPILER)
-  fatal_error("gcc-11 not found")
+  fatal_error("gcc-13 not found")
 endif()
 
 if(NOT CMAKE_CXX_COMPILER)
-  fatal_error("g++-11 not found")
+  fatal_error("g++-13 not found")
 endif()
 
 set(
--- cmake/toolchain/gcc-13_cxx20.cmake
+++ cmake/toolchain/gcc-13_cxx20.cmake
@@ -1,3 +1,3 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../print.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/compiler/gcc-11.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/compiler/gcc-13.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/cxx20.cmake)
