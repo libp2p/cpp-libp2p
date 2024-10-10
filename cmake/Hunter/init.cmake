4973d4778427e0d4eb37432f922d1aae72ffe0e6
kamilsa <kamilsa16@gmail.com>
2024-10-10 13:30:40 +0500

    Bump hunter

--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -31,7 +31,7 @@ set(
 include(${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake)
 
 HunterGate(
-    URL  https://github.com/qdrvm/hunter/archive/refs/tags/v0.25.3-qdrvm22.zip
-    SHA1 72b046fe42baefb968898e46f9442a7e9bc4e1b0
+    URL  https://github.com/qdrvm/hunter/archive/ba0d350bbf272bb0a8c39885ce58fb5230f6b41e.zip
+    SHA1 8d1c44e36334ca538150d8e549b8a526f1cf920f
     LOCAL
 )
