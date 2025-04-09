a5981175ad1a71bd80e33f71c19e71dbd12da01b
Boris Erakhtin <boris.erakhtin@gmail.com>
2025-04-09 20:40:25 +0500

    Fixing overflow of timepoints addition

--- src/peer/address_repository/inmem_address_repository.cpp
+++ src/peer/address_repository/inmem_address_repository.cpp
@@ -64,6 +64,15 @@ namespace libp2p::peer {
     return false;
   }
 
+  Clock::time_point InmemAddressRepository::calculateExpirationTime(
+      const Milliseconds &ttl) const {
+    if (ttl >= std::chrono::milliseconds::max() / 2) {
+      // Handle "permanent" or extremely long TTL values safely
+      return Clock::time_point::max();
+    }
+    return Clock::now() + ttl;
+  }
+
   outcome::result<bool> InmemAddressRepository::addAddresses(
       const PeerId &p,
       std::span<const multi::Multiaddress> ma,
@@ -71,7 +80,8 @@ namespace libp2p::peer {
     bool added = false;
     auto &addresses = db_[p];
 
-    auto expires_at = Clock::now() + ttl;
+    auto expires_at = calculateExpirationTime(ttl);
+
     for (const auto &m : ma) {
       if (addresses.expires.emplace(m, expires_at).second) {
         addresses.order.emplace_back(m);
@@ -90,7 +100,8 @@ namespace libp2p::peer {
     bool added = false;
     auto &addresses = db_[p];
 
-    auto expires_at = Clock::now() + ttl;
+    auto expires_at = calculateExpirationTime(ttl);
+
     for (const auto &m : ma) {
       auto [addr_it, emplaced] = addresses.expires.emplace(m, expires_at);
       if (emplaced) {
@@ -113,7 +124,8 @@ namespace libp2p::peer {
     }
     auto &addresses = peer_it->second;
 
-    auto expires_at = Clock::now() + ttl;
+    auto expires_at = calculateExpirationTime(ttl);
+
     for (auto &item : addresses.expires) {
       item.second = expires_at;
     }
