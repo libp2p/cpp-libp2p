1c033393286f9d2d2360ca6638d5710820375e09
kamilsa <kamilsa16@gmail.com>
2025-04-24 17:56:44 +0500

    Hunter deps cleanup (#307)
    * Use fork of ZLIB when compile on MAC
    
    * Remove unnecessary CMAKE_ARGS for ZLIB configuration
    
    * Try build with hunter where default zlib is updated
    
    * Update qtils version to 0.1.1 and update HunterGate URL and SHA1
    
    * Update HunterGate URL
    
    * Update HunterGate URL
    
    * Update HunterGate URL

--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -31,7 +31,7 @@ set(
 include(${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake)
 
 HunterGate(
-    URL  https://github.com/qdrvm/hunter/archive/refs/tags/v0.25.3-qdrvm28.tar.gz
-    SHA1 a4f1b0f42464e07790b7f90b783a822d71be6c6d
+    URL  https://github.com/qdrvm/hunter/archive/refs/tags/v0.25.3-qdrvm36.tar.gz
+    SHA1 15ca50249cd99e107cb06dc5978fe71118d79a43
     LOCAL
 )
